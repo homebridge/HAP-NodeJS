a486910e0737578cd71547aa283b6e629e202ef0
Alistair Galbraith <alistair@alistairs.net>
2015-07-03 17:54:09 -0700

    Added Contact and Motion Service types, Contact Sensor Characteristic Type (iOS9)

--- accessories/types.js
+++ accessories/types.js
@@ -20,6 +20,8 @@ exports.LOCK_MANAGEMENT_STYPE = stPre + "44" + stPost;
 exports.ALARM_STYPE = stPre + "7E" + stPost;
 exports.WINDOW_COVERING_STYPE = stPre + "8C" + stPost;
 exports.OCCUPANCY_SENSOR_STYPE = stPre + "86" + stPost;
+exports.CONTACT_SENSOR_STYPE = stPre + "80" + stPost;
+exports.MOTION_SENSOR_STYPE = stPre + "85" + stPost;
 
 //HomeKitCharacteristicsTypes
 
@@ -30,6 +32,7 @@ exports.ADMIN_ONLY_ACCESS_CTYPE = stPre + "01" + stPost;
 exports.AUDIO_FEEDBACK_CTYPE = stPre + "05" + stPost;
 exports.BRIGHTNESS_CTYPE = stPre + "08" + stPost;
 exports.COOLING_THRESHOLD_CTYPE = stPre + "0D" + stPost;
+exports.CONTACT_SENSOR_STATE_CTYPE = stPre + "6A" + stPost;
 exports.CURRENT_DOOR_STATE_CTYPE = stPre + "0E" + stPost;
 exports.CURRENT_LOCK_MECHANISM_STATE_CTYPE = stPre + "1D" + stPost;
 exports.CURRENT_RELATIVE_HUMIDITY_CTYPE = stPre + "10" + stPost;
