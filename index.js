96414ff690e5e4d1e0bb23318f82aadbb2ed8bcb
Nick Farina <nfarina@gmail.com>
2015-08-04 16:44:05 -0700

    Add init method for storage options

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ var Service = require('./lib/Service.js').Service;
 var Characteristic = require('./lib/Characteristic.js').Characteristic;
 var uuid = require('./lib/util/uuid');
 var AccessoryLoader = require('./lib/AccessoryLoader.js');
+var storage = require('node-persist');
 
 // ensure Characteristic subclasses are defined
 var HomeKitTypes = require('./lib/gen/HomeKitTypes');
 
 module.exports = {
+  init: init,
   Accessory: Accessory,
   Bridge: Bridge,
   Service: Service,
   Characteristic: Characteristic,
   uuid: uuid,
   AccessoryLoader: AccessoryLoader
+}
+
+function init(storagePath) {
+  // initialize our underlying storage system, passing on the directory if needed
+  if (typeof storagePath !== 'undefined')
+    storage.initSync({ dir: storagePath });
+  else
+    storage.initSync(); // use whatever is default
 }
