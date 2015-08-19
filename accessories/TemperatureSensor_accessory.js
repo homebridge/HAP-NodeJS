910677ea7387f99d47166359aaeeaf171272f392
Nick Farina <nfarina@gmail.com>
2015-08-19 14:34:18 -0700

    Enhance temperature sensor example
    Create a fake sensor that randomizes temperature every 3 seconds and
    updates the characteristic value for subscribed iOS clients.

--- accessories/TemperatureSensor_accessory.js
+++ accessories/TemperatureSensor_accessory.js
@@ -3,6 +3,20 @@ var Service = require('../').Service;
 var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 
+// here's a fake temperature sensor device that we'll expose to HomeKit
+var FAKE_SENSOR = {
+  currentTemperature: 50,
+  getTemperature: function() { 
+    console.log("Getting the current temperature!");
+    return FAKE_SENSOR.currentTemperature;
+  },
+  randomizeTemperature: function() {
+    // randomize temperature to a value between 0 and 100
+    FAKE_SENSOR.currentTemperature = Math.round(Math.random() * 100);
+  }
+}
+
+
 // Generate a consistent UUID for our Temperature Sensor Accessory that will remain the same
 // even when restarting our server. We use the `uuid.generate` helper function to create
 // a deterministic UUID based on an arbitrary "namespace" and the string "temperature-sensor".
@@ -20,9 +34,20 @@ sensor.pincode = "031-45-154";
 sensor
   .addService(new Service.TemperatureSensor())
   .getCharacteristic(Characteristic.CurrentTemperature)
-  .setValue(66) // set a default initial value of 66 degrees C
   .on('get', function(callback) {
     
-    // for now we'll return a hardcoded value of 77 degrees C (~170 degrees F)
-    callback(null, 77);
+    // return our current value
+    callback(null, FAKE_SENSOR.getTemperature());
   });
+
+// randomize our temperature reading every 3 seconds
+setInterval(function() {
+  
+  FAKE_SENSOR.randomizeTemperature();
+  
+  // update the characteristic value so interested iOS devices can get notified
+  sensor
+    .getService(Service.TemperatureSensor)
+    .setCharacteristic(Characteristic.CurrentTemperature, FAKE_SENSOR.currentTemperature);
+  
+}, 3000);
