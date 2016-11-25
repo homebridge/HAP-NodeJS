12091cbc28b14594bf735b53ea2c0338e4f247c1
Khaos Tian <khaos.tian@gmail.com>
2016-11-25 15:32:37 -0800

    Update the example

--- accessories/Light_accessory.js
+++ accessories/Light_accessory.js
@@ -3,90 +3,152 @@ var Service = require('../').Service;
 var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 
-// here's a fake hardware device that we'll expose to HomeKit
-var FAKE_LIGHT = {
-  powerOn: false,
-  brightness: 100, // percentage
-  
-  setPowerOn: function(on) { 
-    console.log("Turning the light %s!", on ? "on" : "off");
-    FAKE_LIGHT.powerOn = on;
+var LightController = {
+  name: "Simple Light", //name of accessory
+  pincode: "031-45-154",
+  username: "FA:3C:ED:5A:1A:1A", // MAC like address used by HomeKit to differentiate accessories. 
+  manufacturer: "HAP-NodeJS", //manufacturer (optional)
+  model: "v1.0", //model (optional)
+  serialNumber: "A12S345KGB", //serial number (optional)
+
+  power: false, //curent power status
+  brightness: 100, //current brightness
+  hue: 0, //current hue
+  saturation: 0, //current saturation
+
+  outputLogs: false, //output logs
+
+  setPower: function(status) { //set power of accessory
+    if(this.outputLogs) console.log("Turning the '%s' %s", this.name, status ? "on" : "off");
+    this.power = status;
   },
-  setBrightness: function(brightness) {
-    console.log("Setting light brightness to %s", brightness);
-    FAKE_LIGHT.brightness = brightness;
+
+  getPower: function() { //get power of accessory
+    if(this.outputLogs) console.log("'%s' is %s.", this.name, this.power ? "on" : "off");
+    return this.power ? true : false;
   },
-  identify: function() {
-    console.log("Identify the light!");
+
+  setBrightness: function(brightness) { //set brightness
+    if(this.outputLogs) console.log("Setting '%s' brightness to %s", this.name, brightness);
+    this.brightness = brightness;
+  },
+
+  getBrightness: function() { //get brightness
+    if(this.outputLogs) console.log("'%s' brightness is %s", this.name, this.brightness);
+    return this.brightness;
+  },
+
+  setSaturation: function(saturation) { //set brightness
+    if(this.outputLogs) console.log("Setting '%s' saturation to %s", this.name, saturation);
+    this.saturation = saturation;
+  },
+
+  getSaturation: function() { //get brightness
+    if(this.outputLogs) console.log("'%s' saturation is %s", this.name, this.saturation);
+    return this.saturation;
+  },
+
+  setHue: function(hue) { //set brightness
+    if(this.outputLogs) console.log("Setting '%s' hue to %s", this.name, hue);
+    this.hue = hue;
+  },
+
+  getHue: function() { //get hue
+    if(this.outputLogs) console.log("'%s' hue is %s", this.name, this.hue);
+    return this.hue;
+  },
+
+  identify: function() { //identify the accessory
+    if(this.outputLogs) console.log("Identify the '%s'", this.name);
   }
 }
 
 // Generate a consistent UUID for our light Accessory that will remain the same even when
 // restarting our server. We use the `uuid.generate` helper function to create a deterministic
 // UUID based on an arbitrary "namespace" and the word "light".
-var lightUUID = uuid.generate('hap-nodejs:accessories:light');
+var lightUUID = uuid.generate('hap-nodejs:accessories:light' + LightController.name);
 
-// This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
-var light = exports.accessory = new Accessory('Light', lightUUID);
+// This is the Accessory that we'll return to HAP-NodeJS that represents our light.
+var lightAccessory = exports.accessory = new Accessory(LightController.name, lightUUID);
 
 // Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-light.username = "1A:2B:3C:4D:5E:FF";
-light.pincode = "031-45-154";
+lightAccessory.username = LightController.username;
+lightAccessory.pincode = LightController.pincode;
 
 // set some basic properties (these values are arbitrary and setting them is optional)
-light
+lightAccessory
   .getService(Service.AccessoryInformation)
-  .setCharacteristic(Characteristic.Manufacturer, "Oltica")
-  .setCharacteristic(Characteristic.Model, "Rev-1")
-  .setCharacteristic(Characteristic.SerialNumber, "A1S2NASF88EW");
+    .setCharacteristic(Characteristic.Manufacturer, LightController.manufacturer)
+    .setCharacteristic(Characteristic.Model, LightController.model)
+    .setCharacteristic(Characteristic.SerialNumber, LightController.serialNumber);
 
 // listen for the "identify" event for this Accessory
-light.on('identify', function(paired, callback) {
-  FAKE_LIGHT.identify();
-  callback(); // success
+lightAccessory.on('identify', function(paired, callback) {
+  LightController.identify();
+  callback();
 });
 
 // Add the actual Lightbulb Service and listen for change events from iOS.
 // We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
-light
-  .addService(Service.Lightbulb, "Fake Light") // services exposed to the user should have "names" like "Fake Light" for us
+lightAccessory
+  .addService(Service.Lightbulb, LightController.name) // services exposed to the user should have "names" like "Light" for this case
   .getCharacteristic(Characteristic.On)
   .on('set', function(value, callback) {
-    FAKE_LIGHT.setPowerOn(value);
-    callback(); // Our fake Light is synchronous - this value has been successfully set
-  });
+    LightController.setPower(value);
 
-// We want to intercept requests for our current power state so we can query the hardware itself instead of
-// allowing HAP-NodeJS to return the cached Characteristic.value.
-light
-  .getService(Service.Lightbulb)
-  .getCharacteristic(Characteristic.On)
-  .on('get', function(callback) {
-    
-    // this event is emitted when you ask Siri directly whether your light is on or not. you might query
-    // the light hardware itself to find this out, then call the callback. But if you take longer than a
-    // few seconds to respond, Siri will give up.
-    
-    var err = null; // in case there were any problems
-    
-    if (FAKE_LIGHT.powerOn) {
-      console.log("Are we on? Yes.");
-      callback(err, true);
-    }
-    else {
-      console.log("Are we on? No.");
-      callback(err, false);
-    }
-  });
-
-// also add an "optional" Characteristic for Brightness
-light
-  .getService(Service.Lightbulb)
-  .addCharacteristic(Characteristic.Brightness)
-  .on('get', function(callback) {
-    callback(null, FAKE_LIGHT.brightness);
-  })
-  .on('set', function(value, callback) {
-    FAKE_LIGHT.setBrightness(value);
+    // Our light is synchronous - this value has been successfully set
+    // Invoke the callback when you finished processing the request
+    // If it's going to take more than 1s to finish the request, try to invoke the callback
+    // after getting the request instead of after finishing it. This avoids blocking other
+    // requests from HomeKit.
     callback();
   })
+  // We want to intercept requests for our current power state so we can query the hardware itself instead of
+  // allowing HAP-NodeJS to return the cached Characteristic.value.
+  .on('get', function(callback) {
+    callback(null, LightController.getPower());
+  });
+
+// To inform HomeKit about changes occurred outside of HomeKit (like user physically turn on the light)
+// Please use Characteristic.updateValue
+// 
+// lightAccessory
+//   .getService(Service.Lightbulb)
+//   .getCharacteristic(Characteristic.On)
+//   .updateValue(true);
+
+// also add an "optional" Characteristic for Brightness
+lightAccessory
+  .getService(Service.Lightbulb)
+  .addCharacteristic(Characteristic.Brightness)
+  .on('set', function(value, callback) {
+    LightController.setBrightness(value);
+    callback();
+  })
+  .on('get', function(callback) {
+    callback(null, LightController.getBrightness());
+  });
+
+// also add an "optional" Characteristic for Saturation
+lightAccessory
+  .getService(Service.Lightbulb)
+  .addCharacteristic(Characteristic.Saturation)
+  .on('set', function(value, callback) {
+    LightController.setSaturation(value);
+    callback();
+  })
+  .on('get', function(callback) {
+    callback(null, LightController.getSaturation());
+  });
+
+// also add an "optional" Characteristic for Hue
+lightAccessory
+  .getService(Service.Lightbulb)
+  .addCharacteristic(Characteristic.Hue)
+  .on('set', function(value, callback) {
+    LightController.setHue(value);
+    callback();
+  })
+  .on('get', function(callback) {
+    callback(null, LightController.getHue());
+  });
