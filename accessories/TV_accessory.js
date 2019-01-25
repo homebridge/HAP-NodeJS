e052f479dc53402fbfa5cc31783ecc54eb589448
Tian Zhang <khaos.tian@gmail.com>
2019-01-24 22:49:25 -0800

    Fix copy paste issue...

--- accessories/TV_accessory.js
+++ accessories/TV_accessory.js
@@ -3,7 +3,7 @@ var Service = require('../').Service;
 var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 
-// Generate a consistent UUID for our Motion Sensor Accessory that will remain the same even when
+// Generate a consistent UUID for TV that will remain the same even when
 // restarting our server. We use the `uuid.generate` helper function to create a deterministic
 // UUID based on an arbitrary "namespace" and the word "tv".
 var tvUUID = uuid.generate('hap-nodejs:accessories:tv');
