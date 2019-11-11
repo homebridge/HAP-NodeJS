f508351e03629eb2dd379105c9ba0bcafb2cfe1b
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2019-11-11 20:47:02 +1300

    Add Wi-Fi router accessory and fix naming conventions (#749)
    * Change Wi-Fi Satellite to match HomeKit naming conventions
    
    * Add Wi-Fi Router accessory

--- src/accessories/Wi-FiSatellite_accessory.ts
+++ src/accessories/Wi-FiSatellite_accessory.ts
@@ -1,6 +1,7 @@
 import {
   Accessory,
   AccessoryEventTypes,
+  Categories,
   Characteristic,
   Service,
   uuid,
@@ -8,12 +9,14 @@ import {
 } from '..';
 
 const UUID = uuid.generate('hap-nodejs:accessories:wifi-satellite');
-export const accessory = new Accessory('Test Wi-Fi Network', UUID);
+export const accessory = new Accessory('Wi-Fi Satellite', UUID);
 
 // @ts-ignore
 accessory.username = 'FA:3C:ED:5A:1A:A2';
 // @ts-ignore
 accessory.pincode = '031-45-154';
+// @ts-ignore
+accessory.category = Categories.ROUTER;
 
 accessory.on(AccessoryEventTypes.IDENTIFY, (paired: boolean, callback: VoidCallback) => {
   console.log("Identify the '%s'", accessory.displayName);
