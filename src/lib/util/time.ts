184ca8562536dec702f6c9d7030b97c70e2026ee
Supereg <mail@anderl-bauer.de>
2022-09-21 15:47:39 +0900

    Add tlv test cases

--- src/lib/util/time.ts
+++ src/lib/util/time.ts
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { readUInt64BE } from "./tlv";
+import { readUInt64LE } from "./tlv";
 
 const EPOCH_MILLIS_2001_01_01 = Date.UTC(2001, 0, 1, 0, 0, 0, 0);
 
@@ -9,6 +9,6 @@ export function epochMillisFromMillisSince2001_01_01(millis: number): number {
 
 export function epochMillisFromMillisSince2001_01_01Buffer(millis: Buffer): number {
   assert(millis.length === 8, "can only parse 64 bit buffers!");
-  const millisSince2001 = readUInt64BE(millis);
+  const millisSince2001 = readUInt64LE(millis);
   return epochMillisFromMillisSince2001_01_01(millisSince2001);
 }
