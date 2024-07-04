5e9ec8bec6e936314e48c354a88658215acd68a2
Donavan Becker <beckersmarthome@icloud.com>
2024-07-04 00:02:29 -0500

    Update checkName.ts

--- src/lib/util/checkName.ts
+++ src/lib/util/checkName.ts
@@ -12,7 +12,7 @@ export function checkName(displayName: string, name: string, value: Nullable<Cha
   // Ensure the string starts and ends with a Unicode letter or number and allow any combination of letters, numbers, spaces, and apostrophes in the middle.
   if (typeof value === "string" && !(new RegExp(/^[\p{L}\p{N}][\p{L}\p{N} ']*[\p{L}\p{N}]$/u)).test(value)) {
     console.warn("HAP-NodeJS WARNING: The accessory '" + displayName + "' is getting published with the characteristic '" +
-      name + "'" + " not following HomeKit naming rules ('" + value + "')," + " with a pattern that ('" + pattern + "'). " +
+      name + "'" + " not following HomeKit naming rules ('" + value + "'). " +
       "Use only alphanumeric, space, and apostrophe characters, start and end with an alphabetic or numeric character, and don't include emojis. " +
       "This might prevent the accessory from being added to the Home App or leading to the accessory being unresponsive!");
   }
